b8c634d34027f3fd0f8a313d2e4cfa799d4442fc
Rohit <108233235+TRohit20@users.noreply.github.com>
2022-10-20 21:16:18 +0530

    Pointer and Logic

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import "fmt"
 func main(){
 	var conferenceName = "Rohit's Crowd Work"
 	const conferenceTickets = 50
-	var remainingTickets = 50
+	var remainingTickets uint = 50
 	// An alternative i.e Syntactic Sugar, IT ONLY APPLIES TO VAR NOT CONST/CONSTANTS
 	// conferenceName := "Rohit"
 
@@ -19,15 +19,30 @@ func main(){
 	fmt.Println("Hurry up! & Get your tickets here")
 
 	// Decalartion
-	var userName string
-	var userTickets int
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
 	// Ask fro user input or Initialisation
 	// userName = "Tom"
 	// userTickets = 2
 
 	// Taking a input, We do that using scan func and pointer 
-	fmt.Scan(&userName)
+	fmt.Println("Enter your First name:")
+	fmt.Scan(&firstName)
+	fmt.Println("Enter your Last name:")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your E-mail:")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter how many tickets you'd like to purchase:")
 	fmt.Scan(&userTickets)
-	fmt.Printf("The user %v booked %v tickets. \n",	userName,userTickets)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets \n",firstName,lastName,userTickets)
+	fmt.Printf("You will receive a confirmation mail to your mail ID %v \n",email)
+
+	remainingTickets -= userTickets
+	fmt.Printf("only %v tickets remaining \n",remainingTickets)
 
 }
