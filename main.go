6208f90cf9dcb7326d386069a2d521a3ecd8a36a
Rohit <108233235+TRohit20@users.noreply.github.com>
2022-10-20 20:26:57 +0530

    File Structure

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+import "fmt"
+
+// HOw do you know which input or file we need is in which package?
+// Google it or simple read in the GOlang Documentaion
+
+func main(){
+	fmt.Print("Hi, my name is Rohit")
+}
