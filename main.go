79032be8d09868120968a12a2d56f4715622eb5d
Rohit <108233235+TRohit20@users.noreply.github.com>
2022-10-20 20:39:28 +0530

    Printf

--- main.go
+++ main.go
@@ -1,4 +1,5 @@
 package main
+
 import "fmt"
 
 // HOw do you know which input or file we need is in which package?
@@ -7,8 +8,12 @@ import "fmt"
 func main(){
 	var conferenceName = "Rohit's Crowd Work"
 	const conferenceTickets = 50
+	var remainingTickets = 50
 
-	fmt.Println("Welcome to ", conferenceName, "booking Application")
+	// fmt.Println("Welcome to ", conferenceName, "booking Application")
+	// Printf instead of 'ln'
+	fmt.Printf("Welcome to %v booking application \n",conferenceName)
+	fmt.Println("We have a total of", conferenceTickets,"tickets, of which only ",remainingTickets, " are remaining")
 	fmt.Println("Hurry up! & Get your tickets here")
 
 
