aabc0b626775ebacc3d2652095f4cc37ba42ab81
Rohit <108233235+TRohit20@users.noreply.github.com>
2022-10-21 13:51:34 +0530

    For loop

--- main.go
+++ main.go
@@ -34,32 +34,38 @@ func main(){
 	// userTickets = 2
 
 	// Taking a input, We do that using scan func and pointer 
-	fmt.Println("Enter your First name:")
-	fmt.Scan(&firstName)
-	fmt.Println("Enter your Last name:")
-	fmt.Scan(&lastName)
 
-	fmt.Println("Enter your E-mail:")
-	fmt.Scan(&email)
+	for{
+		fmt.Println("Enter your First name:")
+		fmt.Scan(&firstName)
+		fmt.Println("Enter your Last name:")
+		fmt.Scan(&lastName)
 
-	fmt.Println("Enter how many tickets you'd like to purchase:")
-	fmt.Scan(&userTickets)
+		fmt.Println("Enter your E-mail:")
+		fmt.Scan(&email)
 
-	fmt.Printf("Thank you %v %v for booking %v tickets \n",firstName,lastName,userTickets)
-	fmt.Printf("You will receive a confirmation mail to your mail ID %v \n",email)
+		fmt.Println("Enter how many tickets you'd like to purchase:")
+		fmt.Scan(&userTickets)
 
-	// bookings[0] = firstName + " " + lastName 
+		fmt.Printf("Thank you %v %v for booking %v tickets \n",firstName,lastName,userTickets)
+		fmt.Printf("You will receive a confirmation mail to your mail ID %v \n",email)
 
-	bookings = append(bookings, firstName + " " + lastName)
+		bookings = append(bookings, firstName + " " + lastName)
+		
+		remainingTickets -= userTickets
+
+		fmt.Printf("only %v tickets remaining \n",remainingTickets)
+
+		fmt.Printf("These are all the bookings %v \n",bookings)	
+	}
+
 	
-	remainingTickets -= userTickets
 
-	fmt.Printf("only %v tickets remaining \n",remainingTickets)
-
+	
+	// bookings[0] = firstName + " " + lastName 
 	// fmt.Printf("The elements in the Slice: %v \n",bookings)
 	// fmt.Printf("The first value in the slice is: %v \n",bookings[0])
 	// fmt.Printf("The length of slice is: %v \n",len(bookings))
 	// fmt.Printf("Type of the Slice being: %T \n",bookings)
 
-	fmt.Printf("These are all the bookings %v \n",bookings)
 }
