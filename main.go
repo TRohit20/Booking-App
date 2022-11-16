e9a526bddc3d129bba97543fd027ef779b53eee1
Rohit <108233235+TRohit20@users.noreply.github.com>
2022-11-16 23:19:01 +0530

    Maps, Struct, Goroutines and Concurrency

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
-	"strconv"
-	"strings"
+	"sync"
+	"time"
+	// "strconv"
+	// "strings"
 )
 
 // Package level variables declaration
@@ -12,10 +14,21 @@ var conferenceName = "Rohit's Crowd Work"
 const conferenceTickets = 50
 
 var RemainingTickets uint = 50
-var bookings = make([]map[string]string, 0)
+
+// var bookings = make(map[string]string, 0)
+var bookings = make([]userData, 0)
+
+type userData struct {
+	firstName   string
+	lastName    string
+	email       string
+	noOfTickets uint
+}
 
 // HOw do you know which input or file we need is in which package?
 // Google it or simple read in the GOlang Documentaion
+
+var syncing = sync.WaitGroup{}
 
 func main() {
 
@@ -32,38 +45,38 @@ func main() {
 	// fmt.Printf("Welcome to %v booking application \n", conferenceName)
 	// Taking a input, We do that using scan func and pointer
 
-	for RemainingTickets > 0 || len(bookings) < 50 {
+	firstName, lastName, email, userTickets := takeInput()
+	validName, validMail, validTickets := UserInValid(firstName, lastName, email, userTickets, RemainingTickets)
 
-		firstName, lastName, email, userTickets := takeInput()
-		validName, validMail, validTickets := UserInValid(firstName, lastName, email, userTickets, RemainingTickets)
+	if validMail && validName && validTickets {
 
-		if validMail && validName && validTickets {
+		bookTicket(firstName, lastName, userTickets, email)
 
-			bookTicket(firstName, lastName, userTickets, email)
+		syncing.Add(1)
+		go sendTicket(userTickets, firstName, lastName, email)
 
-			// Calling a function to go through the bookings array to print names of bookings
-			firstNames := bNames()
-			fmt.Printf("These are the First names of the bookings: %v \n", firstNames)
+		// Calling a function to go through the bookings array to print names of bookings
+		firstNames := bNames()
+		fmt.Printf("These are the First names of the bookings: %v \n", firstNames)
 
-			// Boolean expression
-			// var soldOut bool = remainingTickets == 0
-			soldOut := RemainingTickets == 0
-			if soldOut {
-				fmt.Println("Sorry, The tickets for the conference are all sold. Better luck next time")
-				break
-			}
-		} else {
-			if !validName {
-				fmt.Println("Please enter a valid name ")
-			}
-			if !validMail {
-				fmt.Println("Please provide a valid email ID")
-			}
-			if !validTickets {
-				fmt.Println("PLease enter positive number of tickets")
-			}
-			continue
+		// Boolean expression
+		// var soldOut bool = remainingTickets == 0
+		soldOut := RemainingTickets == 0
+		if soldOut {
+			fmt.Println("Sorry, The tickets for the conference are all sold. Better luck next time")
+
 		}
+	} else {
+		if !validName {
+			fmt.Println("Please enter a valid name ")
+		}
+		if !validMail {
+			fmt.Println("Please provide a valid email ID")
+		}
+		if !validTickets {
+			fmt.Println("PLease enter positive number of tickets")
+		}
+		syncing.Wait()
 	}
 
 	// bookings[0] = firstName + " " + lastName
@@ -116,26 +129,37 @@ func takeInput() (string, string, string, uint) {
 func bNames() []string {
 	firstNames := []string{}
 	for _, booking := range bookings {
-		booking["firstName"]
-		var names = strings.Fields(booking)
+
+		// var names = strings.Fields(booking)
 		// var firstName = names[0]
-		firstNames = append(firstNames, names[0])
+		firstNames = append(firstNames, booking.firstName)
 	}
 	return firstNames
 }
 
 func bookTicket(firstName string, lastName string, userTickets uint, email string) {
 	// Creating a MAP for a user
-	var userData = make(map[string]string)
-	userData["firstName"] = firstName
-	userData["lastName"] = lastName
-	userData["email"] = email
-	userData["noOfTickets"] = strconv.FormatUint(uint64(userTickets), 10)
+	var userData = userData{
+		firstName:   firstName,
+		lastName:    lastName,
+		email:       email,
+		noOfTickets: userTickets,
+	}
 
 	bookings = append(bookings, userData)
+	fmt.Printf("LIST OF ALL BOOKINGS IS %v \n", bookings)
 	RemainingTickets -= userTickets
 
 	fmt.Printf("Thank you %v %v for booking %v tickets \n", firstName, lastName, userTickets)
 	fmt.Printf("You will receive a confirmation mail to your mail ID %v \n", email)
 	fmt.Printf("only %v tickets remaining \n", RemainingTickets)
+}
+
+func sendTicket(userTickets uint, firstName string, lastName string, email string) {
+	time.Sleep(10 * time.Second)
+	var ticket = fmt.Sprintf("%v tickets have been booked under the name %v %v", userTickets, firstName, lastName)
+	fmt.Println("******")
+	fmt.Printf("Booking confirmed, Sending ticket: %v confirmation to %v", ticket, email)
+	fmt.Println("******")
+	syncing.Done()
 }
